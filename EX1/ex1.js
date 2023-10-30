f0f1fd0114518c222478a54636d96675ce09533d
Luiz Fernando Franco Pigozzo <nandofpigozzops21@gmail.com>
2023-10-30 09:34:49 -0300

    Update ex1.js

--- EX1/ex1.js
+++ EX1/ex1.js
@@ -1,8 +1,6 @@
-// Declaração das variáveis
-
 var nome = "Batata";
 var idade = 5;
 
-// Impressão da frase no console
+
 
 console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos")
