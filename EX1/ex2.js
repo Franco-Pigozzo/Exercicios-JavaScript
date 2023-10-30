bcebbf19164eca97b0891be3b6cb50be4bb493e9
Luiz Fernando Franco Pigozzo <nandofpigozzops21@gmail.com>
2023-10-30 09:35:07 -0300

    Update ex2.js

--- EX1/ex2.js
+++ EX1/ex2.js
@@ -1,16 +1,12 @@
-// Declaração das variáveis
-
-    var numero1 = 15;
+ var numero1 = 15;
     var numero2 = 10;
 
-// Operações matemáticas
 
     var soma = numero1 + numero2;
     var subtracao = numero1 - numero2;
     var multiplicacao = numero1 * numero2;
     var divisao = numero1 / numero2;
 
-// Exibindo os resultados no console
 
     console.log("Soma: " + soma);
     console.log("Subtração: " + subtracao);
