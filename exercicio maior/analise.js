0634d510e1c70a7a4a0571fd4db110a7751cc7c2
Franco-Pigozzo <nandofpigozzops21@gmail.com>
2023-12-18 17:14:29 -0300

    correção de exercicios string

--- exercicio maior/analise.js
+++ exercicio maior/analise.js
@@ -1,11 +1,13 @@
 function analisarString(str) {
-    const quantidadeLetras = str.length;
+    const strinTratada = str.replaceAll(" ", "" ) .replaceAll("," , "") .replaceAll("!", "");
+    const quantLetras = strinTratada.length;
     const stringInvertida = str.split("").reverse().join("");
     const quantidadeA = (str.match(/a/gi) || []).length;
   
-    console.log(`Quantidade de letras: ${quantidadeLetras}`);
+    console.log(`Quantidade de letras: ${quantLetras}`);
     console.log(`String invertida: ${stringInvertida}`);
     console.log(`Quantidade de letras 'a': ${quantidadeA}`);
+    
   }
   
   analisarString("Hello, World!");
